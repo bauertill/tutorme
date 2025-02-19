041ce57396f67a8669f2f1dfaf0cae277d3813f0
Till Bauer <bauertill@gmail.com>
2025-02-19 16:57:31 +0200

    Delete wonky shit

--- _worker.js
+++
@@ -1,9 +0,0 @@
-export default {
-  async fetch(request, env, ctx) {
-    try {
-      return await env.ASSETS.fetch(request);
-    } catch (e) {
-      return new Response(`${e.message}\n${e.stack}`, { status: 500 });
-    }
-  },
-};
