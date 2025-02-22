5392b1811763244f16775b91070e857cc62f1c0f
Max Gerer <max@mxgr.de>
2025-02-22 16:32:41 +0100

    start fresh with create-t3-app

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,31 +1,3 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import { DBAdapter } from "@/core/adapters/dbAdapter";
+import { handlers } from "@/server/auth";
 
-const handler = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  callbacks: {
-    async signIn({ user }) {
-      try {
-        const db = new DBAdapter();
-        const existingUser = await db.getUserByEmail(user.email!);
-        
-        if (!existingUser) {
-          await db.createUser(user.email!, user.name!);
-        }
-        
-        return true;
-      } catch (error) {
-        console.error("Error in signIn callback:", error);
-        return false;
-      }
-    }
-  }
-})
-
-export { handler as GET, handler as POST } 
+export const { GET, POST } = handlers;
