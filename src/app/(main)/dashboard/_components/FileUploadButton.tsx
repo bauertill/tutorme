f4445a5e52e43dd3cb821f6442452b3fdc6e0ae5
Till Bauer <bauertill@gmail.com>
2025-02-25 11:55:43 +0200

    Remove unescaped '

--- src/app/(main)/dashboard/_components/FileUploadButton.tsx
+++ src/app/(main)/dashboard/_components/FileUploadButton.tsx
@@ -53,7 +53,7 @@ export default function UploadModal() {
           {isDragActive ? (
             <p>Drop the files here ...</p>
           ) : (
-            <p>Drag 'n' drop some files here, or click to select files</p>
+            <p>Drag n drop some files here, or click to select files</p>
           )}
         </div>
         {files.length > 0 && (
