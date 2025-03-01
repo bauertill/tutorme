5166257168fe21b67f3c5acc08050e8ead907e18
Till Bauer <bauertill@gmail.com>
2025-03-01 12:20:40 +0200

    Introduce lesson types

--- src/core/learning/learningDomain.ts
+++ src/core/learning/learningDomain.ts
@@ -1,7 +1,7 @@
 import type { DBAdapter } from "../adapters/dbAdapter";
 import type { LLMAdapter } from "../adapters/llmAdapter";
 import type { YouTubeAdapter } from "../adapters/youtubeAdapter";
-import type { EducationalVideo } from "./types";
+import type { EducationalVideo, Lesson } from "./types";
 
 export async function findEducationalVideo(
   conceptId: string,
@@ -15,3 +15,21 @@ export async function findEducationalVideo(
   const bestVideo = await llmAdapter.rankVideos(videos, concept);
   return bestVideo;
 }
+
+
+export async function createLesson(
+  goal: string,
+  conceptId: string,
+  dbAdapter: DBAdapter,
+  llmAdapter: LLMAdapter,
+): Promise<Lesson> {
+  // const lesson = await dbAdapter.createLesson(goal, conceptId);
+  return {
+    id: "1",
+    goal: "Learn about the concept of " + goal,
+    iterations: [],
+  };
+}
+
+
+
--- src/core/learning/types.ts
+++ src/core/learning/types.ts
@@ -13,3 +13,47 @@ export const EducationalVideo = z.object({
 export type EducationalVideo = z.infer<typeof EducationalVideo>;
 
 // @TODO Video detail type
+
+
+
+const LessonExplanationTurn = z.object({
+  type: z.literal("explanation"),
+  text: z.string(),
+});
+export type LessonExplanationTurn = z.infer<typeof LessonExplanationTurn>;
+
+const LessonExerciseTurn = z.object({
+  type: z.literal("exercise"),
+  text: z.string(),
+});
+export type LessonExerciseTurn = z.infer<typeof LessonExerciseTurn>;
+
+const LessonUserInputTurn = z.object({
+  type: z.literal("user_input"),
+  text: z.string(),
+});
+export type LessonUserInputTurn = z.infer<typeof LessonUserInputTurn>;
+
+const LessonTurn = z.union([
+  LessonExplanationTurn,
+  LessonExerciseTurn,
+  LessonUserInputTurn,
+]);
+export type LessonTurn = z.infer<typeof LessonTurn>;
+
+export const LessonIteration = z.object({
+  type: z.string(),
+  turns: z.array(LessonTurn),
+  evaluation: z.string(),
+});
+export type LessonIteration = z.infer<typeof LessonIteration>;
+
+export const Lesson = z.object({
+  id: z.string(),
+  goal: z.string(),
+  iterations: z.array(LessonIteration),
+});
+export type Lesson = z.infer<typeof Lesson>;
+
+
+
