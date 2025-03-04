b8a23dfae8d66ae642cdbb81d93262578a06bcf0
Till Bauer <bauertill@gmail.com>
2025-03-04 13:22:52 +0200

    Create dummy exercisees function

--- src/core/adapters/llmAdapter/lesson/index.ts
+++ src/core/adapters/llmAdapter/lesson/index.ts
@@ -1,4 +1,5 @@
 export { chooseProblemForGoal } from "./chooseProblem";
+export { createDummyExercises } from "./createDummyExercises";
 export { createLessonFromProblem } from "./createLessonFromProblem";
 export { createLessonGoalAndDummyExercise } from "./createLessonGoal";
 export { createLessonTeacherReport } from "./createLessonTeacherReport";
