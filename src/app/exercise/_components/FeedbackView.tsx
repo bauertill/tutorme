d4cf56f7b65599d74feb8de53fcd1308e2da1cf0
Max Gerer <max@mxgr.de>
2025-03-09 21:14:14 +0100

    make feedback dismissable

--- src/app/exercise/_components/FeedbackView.tsx
+++ src/app/exercise/_components/FeedbackView.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { Latex } from "@/app/_components/Latex";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
   Collapsible,
@@ -7,20 +8,25 @@ import {
   CollapsibleTrigger,
 } from "@/components/ui/collapsible";
 import { type EvaluationResult } from "@/core/exercise/exerciseDomain";
-import { Info } from "lucide-react";
-
+import { Info, X } from "lucide-react";
+import { useEffect, useState } from "react";
 export default function FeedbackView({
   evaluationResult,
 }: {
   evaluationResult: EvaluationResult;
 }) {
+  const [isOpen, setIsOpen] = useState(true);
+  useEffect(() => {
+    setIsOpen(true);
+  }, [setIsOpen]);
+  if (!isOpen) return null;
   return (
     <Card className="relative">
       <Collapsible>
         <CollapsibleTrigger className="absolute bottom-4 right-4">
           <Info className="h-4 w-4" />
         </CollapsibleTrigger>
-        <CardHeader>
+        <CardHeader className="flex flex-row items-center justify-between">
           <CardTitle>
             {evaluationResult.hasMistakes
               ? "That's not quite right"
@@ -28,6 +34,9 @@ export default function FeedbackView({
                 ? "That's correct!"
                 : "Keep going!"}
           </CardTitle>
+          <Button variant="ghost" size="icon" onClick={() => setIsOpen(false)}>
+            <X className="h-4 w-4" />
+          </Button>
         </CardHeader>
         <CardContent className="whitespace-pre-wrap pb-16">
           <div className="flex flex-col gap-4">
