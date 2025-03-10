665269f010b5f63255bdeb86270b627316b2844a
Max Gerer <max@mxgr.de>
2025-03-10 07:02:26 +0100

    use redis caching for llm requests

--- src/core/adapters/llmAdapter/model.ts
+++ src/core/adapters/llmAdapter/model.ts
@@ -1,22 +1,32 @@
+import { RedisCache } from "@langchain/community/caches/ioredis";
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import { ChatOpenAI } from "@langchain/openai";
+import { Redis } from "ioredis";
+
+const client = new Redis({});
+
+const cache = new RedisCache(client);
 
 export const model = new ChatOpenAI({
   modelName: "gpt-4o",
   temperature: 0.7,
+  cache,
 });
 
 export const fastModel = new ChatOpenAI({
   modelName: "gpt-4o-mini",
   temperature: 0.7,
+  cache,
 });
 
 export const reasoningModel = new ChatOpenAI({
   modelName: "o3-mini",
+  cache,
 });
 
 export const gemini = new ChatGoogleGenerativeAI({
   model: "gemini-2.0-flash-lite",
   temperature: 0,
   maxRetries: 2,
+  cache,
 });
