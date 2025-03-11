bc5b4cfcb6c299b917c505313cdcd62be486b2df
Till Bauer <bauertill@gmail.com>
2025-03-11 10:34:03 +0200

    Bring in Canvas and Problem

--- src/app/(main)/new/_components/Root.tsx
+++ src/app/(main)/new/_components/Root.tsx
@@ -1,5 +1,7 @@
 "use client";
+import { Canvas } from "@/app/(main)/exercise/_components/Canvas";
 import { AppSidebar } from "@/app/_components/AppSidebar";
+import { Latex } from "@/app/_components/Latex";
 import { Separator } from "@/components/ui/separator";
 import {
   SidebarInset,
@@ -9,6 +11,7 @@ import {
 import { type UserProblem } from "@/core/problem/types";
 import { useState } from "react";
 import { dummyAssignments } from "./dummyData";
+
 export function Root() {
   const [activeProblem, setActiveProblem] = useState<UserProblem | null>(null);
   return (
@@ -22,8 +25,18 @@ export function Root() {
         <header className="sticky top-0 flex h-14 items-center gap-4 border-b bg-background px-4">
           <SidebarTrigger />
           <Separator orientation="vertical" className="h-6" />
+          {activeProblem ? (
+            <Latex>{activeProblem.problem}</Latex>
+          ) : (
+            <span className="text-sm text-muted-foreground">
+              No problem selected
+            </span>
+          )}
         </header>
-        <main className="flex-1 p-4"></main>
+        <main className="flex-1 p-4">
+          {/* @TODO on canvas drawing close the side bar */}
+          <Canvas onCheck={() => console.log("CHECKING.....")} />
+        </main>
       </SidebarInset>
     </SidebarProvider>
   );
