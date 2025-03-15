c7f2e9c9739259cceb68fbafeea3456e8452bde8
Max Gerer <max@mxgr.de>
2025-03-15 13:10:28 +0100

    sync on path changes; improve timing

--- src/app/_components/StateSyncProvider.tsx
+++ src/app/_components/StateSyncProvider.tsx
@@ -3,27 +3,23 @@ import { useStore } from "@/store";
 import { api } from "@/trpc/react";
 import { skipToken } from "@tanstack/react-query";
 import { useSession } from "next-auth/react";
-import { useEffect, useRef } from "react";
+import { useCallback, useEffect } from "react";
+import { useDebouncedCallback } from "use-debounce";
 const SYNC_INTERVAL = 60 * 1000;
 
 export function StateSyncProvider({ children }: { children: React.ReactNode }) {
   const session = useSession();
   const assignmentsLocal = useStore.use.assignments();
+  const pathsLocal = useStore.use.paths();
   const upsertAssignmentsLocal = useStore.use.upsertAssignments();
-  const lastSyncTime = useRef<number>(Date.now());
 
-  const {
-    data: assignmentsOnServer,
-    isSuccess,
-    refetch,
-  } = api.assignment.list.useQuery(
+  const { data: assignmentsOnServer, refetch } = api.assignment.list.useQuery(
     session.data?.user.id ? undefined : skipToken,
   );
 
   const { mutate: syncAssignments } =
     api.assignment.syncAssignments.useMutation({
       onSuccess: () => {
-        lastSyncTime.current = Date.now();
         void refetch();
       },
       onError: (error) => {
@@ -31,30 +27,35 @@ export function StateSyncProvider({ children }: { children: React.ReactNode }) {
       },
     });
 
+  const debouncedSync = useDebouncedCallback(
+    useCallback(() => {
+      if (!session.data?.user.id) {
+        console.log("No user id found, skipping sync");
+        return;
+      }
+      if (!assignmentsOnServer) {
+        console.log("Assignments on server not loaded, skipping sync");
+        return;
+      }
+      console.log("syncing assignments");
+      syncAssignments(assignmentsLocal); // TODO: Don't send entire data, only changes
+    }, [
+      session.data?.user.id,
+      assignmentsLocal,
+      assignmentsOnServer,
+      syncAssignments,
+    ]),
+    SYNC_INTERVAL,
+    { leading: true, trailing: true, maxWait: SYNC_INTERVAL },
+  );
+
   useEffect(() => {
-    if (!session.data?.user.id) {
-      console.log("No user id found, skipping sync");
-      return;
+    if (assignmentsOnServer) {
+      upsertAssignmentsLocal(assignmentsOnServer);
     }
-    if (!isSuccess || !assignmentsOnServer) {
-      console.log("Assignments on server not loaded, skipping sync");
-      return;
-    }
-    upsertAssignmentsLocal(assignmentsOnServer);
-    const timeSinceLastSync = Date.now() - lastSyncTime.current;
-    if (timeSinceLastSync > SYNC_INTERVAL) {
-      console.log("Syncing assignments from local to server");
-      syncAssignments(assignmentsLocal);
-      lastSyncTime.current = Date.now();
-    }
-  }, [
-    assignmentsLocal,
-    session.data?.user.id,
-    isSuccess,
-    assignmentsOnServer,
-    syncAssignments,
-    upsertAssignmentsLocal,
-  ]);
+  }, [assignmentsOnServer, upsertAssignmentsLocal]);
+
+  useEffect(debouncedSync, [pathsLocal, debouncedSync]);
 
   return <>{children}</>;
 }
