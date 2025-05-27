db0416586379ce03bcd11b96b70ef4b893e14dbe
Max Gerer <max@mxgr.de>
2025-05-27 11:55:52 +0200

    Hide scroll finger animation in store after first scroll

--- src/app/(main)/assignment/_components/Canvas/AnimatedScrollIcon.tsx
+++ src/app/(main)/assignment/_components/Canvas/AnimatedScrollIcon.tsx
@@ -1,7 +1,6 @@
 import { cn } from "@/lib/utils";
 import { useStore } from "@/store";
 import { motion } from "motion/react";
-import { useEffect } from "react";
 
 const SVG = () => (
   <svg
@@ -25,17 +24,7 @@ export default function AnimatedScrollIcon(
   props: React.HTMLAttributes<HTMLDivElement>,
 ) {
   const isTourRunning = useStore.use.isTourRunning();
-  const hasShownScrollFingers = useStore.use.hasShownScrollFingers();
-  const setHasShownScrollFingers = useStore.use.setHasShownScrollFingers();
-  console.log("hasShownScrollFingers", hasShownScrollFingers);
-  useEffect(() => {
-    return () => {
-      if (!isTourRunning && !hasShownScrollFingers) {
-        setHasShownScrollFingers(true);
-      }
-    };
-  }, [isTourRunning]);
-  if (isTourRunning || hasShownScrollFingers) return null;
+  if (isTourRunning) return null;
 
   return (
     <div {...props} className={cn("", props.className)}>
