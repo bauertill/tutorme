190c0abc8c7d5aac6597516c3aa64037ef0f427b
Max Gerer <max@mxgr.de>
2025-06-07 19:45:36 +0200

    Fix dialog problems by upgrading radix packages

---
+++ src/hooks/use-dialog.tsx
@@ -0,0 +1,31 @@
+import { useRef, useState } from "react";
+
+export function useDialog() {
+  const [isOpen, setIsOpen] = useState(false);
+  const triggerRef = useRef<HTMLButtonElement>(null);
+
+  function trigger() {
+    setIsOpen(true);
+  }
+
+  function dismiss() {
+    setIsOpen(false);
+    triggerRef.current?.focus();
+  }
+
+  return {
+    triggerProps: {
+      ref: triggerRef,
+      onClick: trigger,
+    },
+    dialogProps: {
+      open: isOpen,
+      onOpenChange: (open: boolean) => {
+        if (open) trigger();
+        else dismiss();
+      },
+    },
+    trigger,
+    dismiss,
+  };
+}
