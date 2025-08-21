e1fc18ca0308c7b0fc9c32f6535708b71a389ae0
Long Truong Le Bao <long.tlb@carehealth.io>
2025-08-22 03:52:57 +0700

    Refactor

--- src/store/problem.selectors.ts
+++ src/store/problem.selectors.ts
@@ -6,12 +6,6 @@ import { useCallback, useMemo } from "react";
 import { useShallow } from "zustand/shallow";
 import { useStore } from ".";
 
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-
 export const useActiveAssignmentId = (): string | null => {
   return useStore(useShallow(({ activeAssignmentId }) => activeAssignmentId));
 };
